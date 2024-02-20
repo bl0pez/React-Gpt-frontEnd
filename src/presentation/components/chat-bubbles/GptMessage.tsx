c9e185aeae0004e910ba2ee9fa8fa76a69c18b04
bryan lopez <blopez4434@gmail.com>
2024-02-20 15:34:44 -0300

    Add prosConsUseCase function and related files

--- src/presentation/components/chat-bubbles/GptMessage.tsx
+++ src/presentation/components/chat-bubbles/GptMessage.tsx
@@ -6,13 +6,13 @@ interface Props {
 
 export const GptMessage = ({ text }: Props) => {
   return (
-      <div className="flex flex-row items-center gap-3">
-        <div className="flex items-center justify-center h-10 w-10 rounded-full bg-green-600 flex-shrink-0">
-          Gpt
-        </div>
-        <div className="relative text-sm bg-black bg-opacity-25 pt-3 pb-2 px-4 shadow rounded-xl">
-          <Markdown>{text}</Markdown>
-        </div>
+    <div className="flex flex-row items-start gap-3">
+      <div className="flex items-center justify-center h-10 w-10 rounded-full bg-green-600 flex-shrink-0">
+        Gpt
       </div>
+      <div className="relative text-sm bg-black bg-opacity-25 pt-3 pb-2 px-4 shadow rounded-xl">
+        <Markdown>{text}</Markdown>
+      </div>
+    </div>
   );
 };
--- src/presentation/pages/pros-cons/ProsConsPage.tsx
+++ src/presentation/pages/pros-cons/ProsConsPage.tsx
@@ -1,5 +1,61 @@
-export const ProsConsPage = () => {
-  return (
-    <div>ProsConsPage</div>
-  )
+import { useState, useEffect, useRef } from "react";
+import {
+  GptMessage,
+  MyMessage,
+  TextMessage,
+  TypingLoader,
+} from "@Components/index";
+import { prosConsUseCase } from "@UseCases/prosCons.use-case";
+
+interface Message {
+  text: string;
+  isGpt: boolean;
 }
+
+export const ProsConsPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+    const { content, ok } = await prosConsUseCase(text);
+    setIsLoading(false);
+
+    if (!ok) return;
+
+    setMessages((prev) => [...prev, { text: content, isGpt: true }]);
+  };
+
+  return (
+    <div className="flex flex-col w-full gap-2">
+      <div className="flex-1 p-4 gap-3 flex flex-col overflow-y-auto">
+        <GptMessage text="Hola, soy un asistente virtual, ¿Qué es lo que quieres comparar?" />
+
+        {messages.map((message, i) =>
+          message.isGpt ? (
+            <GptMessage key={i} text={message.text} />
+          ) : (
+            <MyMessage key={i} text={message.text} />
+          )
+        )}
+
+        <div ref={messagesEndRef} />
+
+        {isLoading && <TypingLoader />}
+      </div>
+
+      <TextMessage
+        onSendMessage={handlePost}
+        placeholder="Escribe tu mensaje aquí"
+        disableCorrections
+      />
+    </div>
+  );
+};
